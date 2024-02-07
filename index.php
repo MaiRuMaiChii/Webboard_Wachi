4dbac70c54212f3126060b676eb26967a738f543
MaiRuMaiChii <neiyneiy456@gmail.com>
2024-02-07 16:51:04 +0700

    Update index.php
    Update index.php

--- index.php
+++ index.php
@@ -6,11 +6,41 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <title>Webboard Shabu</title>
 </head>
 <body>
-    <h1 style="text-align: center;">Webboard Shabu</h1>
-    <hr>
+    <div class="container-lg">
+    <h1 style="text-align: center;" class="mt-3">Webboard Shabu</h1>
+    
+<nav class="navbar navbar-expand-lg " style="background-color: #d3d3d3;">
+  <div class="container-fluid">
+
+    <a class="navbar-brand" href="index.php"><i class="bi bi-house-door-fill"></i> HOME</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+      <ul class="navbar-nav">
+        <?php if(!isset($_SESSION['id'])){?>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="login.php"><i class="bi bi-pencil-square"></i> เข้าสู่ระบบ</a>
+        </li>
+        <?php }else{?>
+        <li class="nav-item dropdown">
+          <a class="btn btn-outline-secondary dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false" ><i class="bi bi-person-lines-fill"></i> 
+            <?php echo $_SESSION['username'];?>
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="logout.php"><i class="bi bi-power"></i> ออกจากระบบ</a></li>
+          </ul>
+        </li>
+        <?php }?>
+      </ul>
+  </div>
+</nav>
+
     <form>
         หมวดหมู่ : 
         <select>
@@ -19,7 +49,7 @@
             <option value="study">เรื่องเรียน</option>
             
         </select>
-        <?php
+         <?php
             if (!isset($_SESSION['id'])){
                echo "<a href=login.php style='float: right;'>เข้าสู่ระบบ</a>";
             }else{
@@ -30,11 +60,11 @@
                 <a href=newpost.php>สร้างกระทู้ใหม่</a>
                 ";
             }
-        ?>
-
-        
+        ?> 
+  
     </form>
     <ul>
+        
         <?php
             for($i=1; $i<=10; $i++){
                 echo "<li><a href=post.php?id=$i>กระทู้ที่ $i </a>";
@@ -44,6 +74,8 @@
                 echo "</li>";
             }
         ?>
+        
     </ul>
+    </div>
 </body>
 </html>
