b8a2b7c7a04d50f4a5fbb80a060535cf9dced628
MaiRuMaiChii <neiyneiy456@gmail.com>
2024-02-21 15:44:37 +0700

    update login.php and verify.php
    update login.php and verify.php

--- verify.php
+++ verify.php
@@ -14,9 +14,8 @@
     <title>Verify</title>
 </head>
 <body>
-    <h1 style="text-align: center;">Webboard Shabu</h1>
-    <hr>
-    <div style="text-align: center;">
+    
+    
     <?php
         $login = $_POST['Login'];
         $pwd = $_POST['pwd'];
@@ -24,16 +23,26 @@
                 $_SESSION["username"] = "admin"; //การสร้างตัวแปรsession  เก็บคำว่าadmin
                 $_SESSION["role"] = "a";
                 $_SESSION["id"] = "session_id()";
-            echo "ยินดีตอนรับคุณ ADMIN";}
+                header("location:index.php");
+            //echo "ยินดีตอนรับคุณ ADMIN";
+        }
         elseif ($login == "member" && $pwd == "mem1234"){
                 $_SESSION["username"] = "member";
                 $_SESSION["role"] = "m";
                 $_SESSION["id"] = "session_id()";
-            echo "ยินดีต้อนรับคุณ MEMBER";}
+                header("location:index.php");
+                die();
+
+            //echo "ยินดีต้อนรับคุณ MEMBER";
+        }
         else{
-            echo "ชื่อบัญชีหรือรหัสผ่านไม่ถูกต้อง";}
+            $_SESSION['error']='error';
+            header("location:login.php");
+            die();
+            //echo "ชื่อบัญชีหรือรหัสผ่านไม่ถูกต้อง";
+        }
     ?>
-    </div>
-    <div style="text-align: center;"><a href="index.php">กลับไปหน้าหลัก</a></div>
+    
+    
 </body>
 </html>
