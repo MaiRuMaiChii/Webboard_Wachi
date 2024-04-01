6a0cc5301804e8dfda9cfc9a3dbbc0d63a13899b
MaiRuMaiChii <152370423+MaiRuMaiChii@users.noreply.github.com>
2024-04-01 12:43:00 +0700

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,18 +1,18 @@
 <?php
-session_start();
-if(isset($_SESSION['id']) && $_SESSION['role'] == 'a'){
-    $id = $_GET['id'];
-    $conn=new PDO("mysql:host=localhost;dbname=webboard;charset=utf8","root","");
-    $sql="DELETE FROM post WHERE id=$id";
-    $conn->exec($sql);
-    $sql="DELETE FROM comment WHERE post_id=$id";
-    $conn->exec($sql);
-    $conn=null;
-    header("location:index.php");
-    die();
-}
-else{
-    header("location:index.php");
-    die();
-}
+    session_start();
+    if(isset($_SESSION['id'])){
+        $conn=new PDO("mysql:host=localhost;dbname=webboard;charset=utf8","root","");
+        $id=$_GET['id'];
+        $sql="DELETE FROM post WHERE id=$id";
+        $conn->exec($sql);
+        $sql="DELETE FROM comment WHERE post_id=$id";
+        $conn->exec($sql);
+        $conn=null;
+        header("location:index.php");
+        die();
+    }else{
+        header("location:index.php");
+        die();
+    }
+    
 ?>
